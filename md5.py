7f50cd1e9ebaf900e3209a44da0076a13c6c5fe2
Kyle Barbre <jamesbarbre@gmail.com>
2023-11-06 21:42:05 -0800

    5th Edition

--- md5.py
+++ md5.py
@@ -13,7 +13,7 @@ def calculate_hash(file_path, hash_algorithm):
                 hasher.update(data)
         return hasher.hexdigest()
     except Exception as e:
-        return f"Error: {str(e)}"
+        return f"Error: {str(e)} (File: {file_path})"
 
 def hash_files_in_directory(directory, hash_algorithm):
     """Calculate hash sums for all files in a directory."""
@@ -27,9 +27,17 @@ def hash_files_in_directory(directory, hash_algorithm):
 
 if __name__ == "__main__":
     directory = "/"  # Use '/' to start from the root directory
-    hash_algorithm = "sha256"  # You can use "md5", "sha1", "sha256", etc.
+    hash_algorithm = "md5"  # You can use "md5", "sha1", "sha256", etc.
 
     hash_sums = hash_files_in_directory(directory, hash_algorithm)
 
-    for file_path, file_hash in hash_sums.items():
-        print(f"{file_path}: {file_hash}")
+    output_directory = "/home/sysadmin/Md5sum"
+    os.makedirs(output_directory, exist_ok=True)
+
+    output_file = os.path.join(output_directory, f"{hash_algorithm}_hashes.txt")
+
+    with open(output_file, "w") as output:
+        for file_path, file_hash in hash_sums.items():
+            output.write(f"{file_path}: {file_hash}\n")
+
+    print(f"Hashes saved to {output_file}")
